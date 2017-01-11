ab90e4b3c04c0148cf02d34aff6114c5591ce92c
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-01-10 22:24:15 -0700

    Style tweaks, render nil case, and fix several regexes in LinkView

--- web/views/v2/link_view.ex
+++ web/views/v2/link_view.ex
@@ -1,8 +1,10 @@
 defmodule Ello.V2.LinkView do
   use Ello.Web, :view
 
-  def render("links.json", links) do
+  def render("links.json", %{links: nil}), do: nil
+  def render("links.json", %{links: links}) do
     links
+    |> String.split(~r/,? +/)
     |> Enum.map(fn link ->
       link
       |> sanitize_url
@@ -21,7 +23,7 @@ defmodule Ello.V2.LinkView do
 
   defp match_url_to_icon_and_format(url) do
     icon_types
-    |> Enum.find(fn(icon_type) -> Regex.match?(icon_type.regex, url) end)
+    |> Enum.find(&Regex.match?(&1.regex, url))
     |> format_link(url)
   end
 
@@ -34,7 +36,7 @@ defmodule Ello.V2.LinkView do
   end
 
   defp social_icon_url(icon_type) do
-    icon_type |> Map.put(:icon, "#{System.get_env("SOCIAL_ICONS_URL")}/#{icon_type.icon}")
+    Map.put(icon_type, :icon, "#{System.get_env("SOCIAL_ICONS_URL")}/#{icon_type.icon}")
   end
 
   defp icon_types do
@@ -49,7 +51,7 @@ defmodule Ello.V2.LinkView do
       %{type: "Ello", icon: "ello.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?ello\.co\/([a-zA-Z0-9_]+)|ello.co\/([a-zA-Z0-9_]+)/},
       %{type: "Etsy", icon: "etsy.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?etsy\.com\/shop\/([a-zA-Z0-9_.-]+)/},
       %{type: "Facebook", icon: "facebook.png", regex: ~r/(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?/},
-      %{type: "500px", icon: "500px.png", regex: ~r/(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?/},
+      %{type: "500px", icon: "500px.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?500px\.com\/([a-zA-Z0-9_]+)|500px.com\/([a-zA-Z0-9_]+)/},
       %{type: "Flickr", icon: "flickr.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?flickr\.com\/photos\/([a-zA-Z0-9_.-]+)/},
       %{type: "Github", icon: "github.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?github\.com\/([a-zA-Z0-9_-]+)|github.com\/([a-zA-Z0-9_]+)/},
       %{type: "Goodreads", icon: "goodreads.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?goodreads\.com\/user\/show\/([a-zA-Z0-9_.-]+)/},
@@ -59,9 +61,9 @@ defmodule Ello.V2.LinkView do
       %{type: "Linkedin", icon: "linkedin.png", regex: ~r/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)/},
       %{type: "Medium", icon: "medium.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?medium\.com\/@([a-zA-Z0-9_.-]+)|medium\.com\/@([a-zA-Z0-9_.-]+)/},
       %{type: "Mixcloud", icon: "mixcloud.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?mixcloud\.com\/([a-zA-Z0-9_]+)|mixcloud\.com\/([a-zA-Z0-9_]+)/},
-      %{type: "Model Mayhem", icon: "model_mayhem.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?mixcloud\.com\/([a-zA-Z0-9_]+)|mixcloud\.com\/([a-zA-Z0-9_]+)/},
+      %{type: "Model Mayhem", icon: "model_mayhem.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?modelmayhem\.com\/([a-zA-Z0-9_-]+)|modelmayhem\.com\/([a-zA-Z0-9_-]+)/},
       %{type: "Patreon", icon: "patreon.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?patreon\.com\/([a-zA-Z0-9_-]+)|patreon\.com\/([a-zA-Z0-9_-]+)/},
-      %{type: "Persona", icon: "persona.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?patreon\.com\/([a-zA-Z0-9_-]+)|patreon\.com\/([a-zA-Z0-9_-]+)/},
+      %{type: "Persona", icon: "persona.png", regex: ~r/[^"\/www\."](?<!w{3})[A-Za-z0-9]*(?=\.persona\.co)/},
       %{type: "Pinterest", icon: "pinterest.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?pinterest\.com\/([a-zA-Z0-9_]+)|pinterest\.com\/([a-zA-Z0-9_]+)/},
       %{type: "Shopify", icon: "shopify.png", regex: ~r/[^"\/www\."](?<!w{3})[A-Za-z0-9]*(?=\.myshopify\.com)/},
       %{type: "Snapchat", icon: "snapchat.png", regex: ~r/http(?:s)?:\/\/(?:www\.)?snapchat\.com\/add\/([a-zA-Z0-9_.-]+)/},
