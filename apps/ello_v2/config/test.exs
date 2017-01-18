a1c460a741407046d0bc323560cde469909ad258
Alan Peabody <gapeabody@gmail.com>
2017-01-18 12:04:31 -0500

    Don't start v2 endpoint server (we don't use it)

--- apps/ello_v2/config/test.exs
+++ apps/ello_v2/config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :ello_v2, Ello.V2.Endpoint,
-  http: [port: 4001],
+  http: [port: nil],
   server: false
 
 config :ello_v2,
