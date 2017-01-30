6db6cbd0c8944ac81359588867cad03ce2b6dc0d
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-01-30 15:01:48 -0700

    Add ClientProperties plug for determining client nsfw/nudity setting

---
+++ apps/ello_v2/lib/ello_v2/client_properties.ex
@@ -0,0 +1,69 @@
+defmodule Ello.V2.ClientProperties do
+  use Plug.Builder
+
+  plug :android
+  plug :ios_version_from_header
+  plug :ios_version_from_ua
+  plug :ios
+  plug :webapp
+  plug :nudity
+  plug :nsfw
+
+  defp android(conn, _) do
+    with [ua | _]  <- get_req_header(conn, "user_agent"),
+         true      <- Regex.match?(~r/Ello Android/i, ua) do
+      assign(conn, :android, true)
+    else
+      _ -> assign(conn, :android, false)
+    end
+  end
+
+  defp ios_version_from_header(%{assigns: %{android: true}} = conn, _),
+    do: conn
+  defp ios_version_from_header(conn, _) do
+    case get_req_header(conn, "x-ios-build-number") do
+      [build | _] -> assign(conn, :ios_version, String.to_integer(build))
+      _           -> conn
+    end
+  end
+
+  defp ios_version_from_ua(%{assigns: %{ios_version: v}} = conn, _)
+    when is_integer(v),
+    do: conn
+  defp ios_version_from_ua(%{assigns: %{android: true}} = conn, _),
+    do: conn
+  defp ios_version_from_ua(conn, _) do
+    with [ua | _]   <- get_req_header(conn, "user_agent"),
+         [_, build] <- Regex.run(~r[^Ello\/(\d{4,6}).*Darwin.*], ua) do
+      assign(conn, :ios_version, String.to_integer(build))
+    else
+      _ -> conn
+    end
+  end
+
+  defp ios(%{assigns: %{ios_version: v}} = conn, _) when is_integer(v),
+    do: assign(conn, :ios, true)
+  defp ios(conn, _),
+    do: assign(conn, :ios, false)
+
+  defp webapp(%{assigns: %{android: false, ios: false}} = conn, _),
+    do: assign(conn, :webapp, true)
+  defp webapp(conn, _),
+    do: assign(conn, :webapp, false)
+
+  defp nudity(%{assigns: %{user: %{settings: %{views_adult_content: true}}}} = conn, _),
+    do: assign(conn, :allow_nudity, true)
+  defp nudity(%{assigns: %{webapp: true}} = conn, _),
+    do: assign(conn, :allow_nudity, true)
+  defp nudity(conn, _),
+    do: assign(conn, :allow_nudity, false)
+
+  defp nsfw(%{assigns: %{user: %{settings: %{views_adult_content: true}}}} = conn, _),
+    do: assign(conn, :allow_nsfw, true)
+  defp nsfw(%{assigns: %{user: %{settings: %{views_adult_content: false}}}} = conn, _),
+    do: assign(conn, :allow_nsfw, false)
+  defp nsfw(%{assigns: %{webapp: true}} = conn, _),
+    do: assign(conn, :allow_nsfw, true)
+  defp nsfw(conn, _),
+    do: assign(conn, :allow_nsfw, false)
+end
---
+++ apps/ello_v2/test/ello_v2/client_properties_test.exs
@@ -0,0 +1,132 @@
+defmodule Ello.V2.ClientPropertiesTest do
+  use Ello.V2.ConnCase
+  alias Ello.V2.ClientProperties
+  alias Ello.Core.Network.User
+
+  test "detects android", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello Android")
+           |> ClientProperties.call([])
+    assert conn.assigns[:android]
+    refute conn.assigns[:ios_version]
+    refute conn.assigns[:ios]
+    refute conn.assigns[:webapp]
+  end
+
+  test "detects ios from user agent", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello/4640 CFNetwork/758.3.15 Darwin/15.5.0")
+           |> ClientProperties.call([])
+    assert conn.assigns[:ios_version] == 4640
+    assert conn.assigns[:ios]
+    refute conn.assigns[:android]
+    refute conn.assigns[:webapp]
+  end
+
+  test "detects ios from build header", %{conn: conn} do
+    conn = conn
+           |> put_req_header("x-ios-build-number", "4640")
+           |> ClientProperties.call([])
+    assert conn.assigns[:ios_version] == 4640
+    assert conn.assigns[:ios]
+    refute conn.assigns[:android]
+    refute conn.assigns[:webapp]
+  end
+
+  test "detects webapp", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Chrome")
+           |> ClientProperties.call([])
+    assert conn.assigns[:webapp]
+    refute conn.assigns[:android]
+    refute conn.assigns[:ios_version]
+    refute conn.assigns[:ios]
+  end
+
+  test "allows nudity/nsfw - no user - webapp", %{conn: conn} do
+    conn = conn
+           |> assign(:webapp, true)
+           |> ClientProperties.call([])
+    assert conn.assigns[:webapp]
+    assert conn.assigns[:allow_nudity]
+    assert conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - no user - android", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello Android")
+           |> ClientProperties.call([])
+    assert conn.assigns[:android]
+    refute conn.assigns[:allow_nudity]
+    refute conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - no user - ios", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello/4640 CFNetwork/758.3.15 Darwin/15.5.0")
+           |> ClientProperties.call([])
+    assert conn.assigns[:ios]
+    refute conn.assigns[:allow_nudity]
+    refute conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - user views nsfw - webapp", %{conn: conn} do
+    conn = conn
+           |> assign(:webapp, true)
+           |> assign(:user, %User{settings: %{views_adult_content: true}})
+           |> ClientProperties.call([])
+    assert conn.assigns[:webapp]
+    assert conn.assigns[:allow_nudity]
+    assert conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - user does not view nsfw - webapp", %{conn: conn} do
+    conn = conn
+           |> assign(:webapp, true)
+           |> assign(:user, %User{settings: %{views_adult_content: false}})
+           |> ClientProperties.call([])
+    assert conn.assigns[:webapp]
+    assert conn.assigns[:allow_nudity]
+    refute conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - user views nsfw - android", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello Android")
+           |> assign(:user, %User{settings: %{views_adult_content: true}})
+           |> ClientProperties.call([])
+    assert conn.assigns[:android]
+    assert conn.assigns[:allow_nudity]
+    assert conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - user does not view nsfw - android", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello Android")
+           |> assign(:user, %User{settings: %{views_adult_content: false}})
+           |> ClientProperties.call([])
+    assert conn.assigns[:android]
+    refute conn.assigns[:allow_nudity]
+    refute conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - user views nsfw - ios", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello/4640 CFNetwork/758.3.15 Darwin/15.5.0")
+           |> assign(:user, %User{settings: %{views_adult_content: true}})
+           |> ClientProperties.call([])
+    assert conn.assigns[:ios]
+    assert conn.assigns[:allow_nudity]
+    assert conn.assigns[:allow_nsfw]
+  end
+
+  test "allows nudity/nsfw - user does not view nsfw - ios", %{conn: conn} do
+    conn = conn
+           |> put_req_header("user_agent", "Ello/4640 CFNetwork/758.3.15 Darwin/15.5.0")
+           |> assign(:user, %User{settings: %{views_adult_content: false}})
+           |> ClientProperties.call([])
+    assert conn.assigns[:ios]
+    refute conn.assigns[:allow_nudity]
+    refute conn.assigns[:allow_nsfw]
+  end
+end
