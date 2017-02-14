72d930f01ea65e66258a4c58edb3c9a18caae553
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-02-14 15:05:56 -0700

    Move asset to content top level directory

--- apps/ello_core/lib/ello_core/content/asset.ex
+++ apps/ello_core/lib/ello_core/content/asset.ex
@@ -1,6 +1,7 @@
 defmodule Ello.Core.Content.Asset do
   use Ecto.Schema
   alias Ello.Core.{ Network.User, Content.Post }
+  alias __MODULE__.Attachment
 
   @type t :: %__MODULE__{}
 
@@ -14,6 +15,13 @@ defmodule Ello.Core.Content.Asset do
 
     belongs_to :user, User
     belongs_to :post, Post
+  end
+
+  @doc """
+  Converts attachment metadata into attachment_struct
+  """
+  def build_attachment(asset) do
+    Map.put(asset, :attachment_struct, Attachment.from_asset(asset))
   end
 
 end
