c3d3a88db2d6a581c32fca90541baea9c357c525
Colin T.A. Gray <colinta@gmail.com>
2017-02-17 11:33:44 -0700

    return 404 from PostController if user_id is passed in, and doesn't match the post author

--- apps/ello_v2/test/controllers/post_controller_test.exs
+++ apps/ello_v2/test/controllers/post_controller_test.exs
@@ -13,6 +13,28 @@ defmodule Ello.PostControllerTest do
     assert json["posts"]["id"] == "#{post.id}"
   end
 
+  test "GET /v2/posts/:id, user_id - success", %{conn: conn, post: post} do
+    conn = get(conn, post_path(conn, :show, post), %{"user_id" => "#{post.author.id}"})
+    json = json_response(conn, 200)
+    assert json["posts"]["id"] == "#{post.id}"
+  end
+
+  test "GET /v2/posts/:id, user_token - success", %{conn: conn, post: post} do
+    conn = get(conn, post_path(conn, :show, post), %{"user_id" => "~#{post.author.username}"})
+    json = json_response(conn, 200)
+    assert json["posts"]["id"] == "#{post.id}"
+  end
+
+  test "GET /v2/posts/:id, user_id - failure", %{conn: conn, post: post} do
+    conn = get(conn, post_path(conn, :show, post), %{"user_id" => "#{post.author.id + 1}"})
+    assert conn.status == 404
+  end
+
+  test "GET /v2/posts/:id, user_token - failure", %{conn: conn, post: post} do
+    conn = get(conn, post_path(conn, :show, post), %{"user_id" => "~i made this up"})
+    assert conn.status == 404
+  end
+
   test "GET /v2/posts/:id 404s", %{conn: conn} do
     conn = get(conn, post_path(conn, :show, "404"))
     assert conn.status == 404
