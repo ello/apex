a474736f67daee69c228af9572d8a0e82977f9b9
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-02-27 11:23:34 -0700

    Grab everything up until the extension before hashing the filename
    - Before, we were string splitting at the “.”; however, there were some
    original filenames that had periods in them which would throw off the
    hash name.

--- apps/ello_core/test/ello_core/image_test.exs
+++ apps/ello_core/test/ello_core/image_test.exs
@@ -37,4 +37,39 @@ defmodule Ello.Core.ImageTest do
     } = hd(Image.Version.from_metadata(metadata, name))
   end
 
+  test "Version.from_metadata/2 - parses metadata when there is a period in original filename" do
+    metadata = %{
+      "large" => %{
+        "size" => 220_669,
+        "type" => "image/png",
+        "width" => 360,
+        "height" => 360
+      },
+      "regular" => %{
+        "size" => 36_629,
+        "type" => "image/png",
+        "width" => 120,
+        "height" => 120
+      },
+      "small" => %{
+        "size" => 17_753,
+        "type" => "image/png",
+        "width" => 60,
+        "height" => 60
+      }
+    }
+    name = "BergerFohr.Mashup.2.Avatar.png"
+
+    assert [%Image.Version{}, %Image.Version{}, %Image.Version{}] = Image.Version.from_metadata(metadata, name)
+    assert %Image.Version{
+      name: "large",
+      width: 360,
+      height: 360,
+      size: 220_669,
+      type: "image/png",
+      filename: "ello-large-a56b875a.png",
+      pixellated_filename: "ello-large-pixellated-a56b875a.png",
+    } = hd(Image.Version.from_metadata(metadata, name))
+  end
+
 end
