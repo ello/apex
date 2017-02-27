bf3c7e850207d5de2745cc0d70bc013710a5d8b5
Alan Peabody <gapeabody@gmail.com>
2017-02-27 08:09:20 -0500

    Don't 500 if missing optimized image.
    Rendering with no seo image is better then 500ing.

--- apps/ello_v2/web/views/user_meta_attributes_view.ex
+++ apps/ello_v2/web/views/user_meta_attributes_view.ex
@@ -18,8 +18,10 @@ defmodule Ello.V2.UserMetaAttributesView do
   defp robots(_), do: "index, follow"
 
   defp image(user) do
-    version = Enum.find(user.cover_image_struct.versions, &(&1.name == "optimized"))
-    image_url(user.cover_image_struct.path, version.filename)
+    case Enum.find(user.cover_image_struct.versions, &(&1.name == "optimized")) do
+      nil     -> nil
+      version -> image_url(user.cover_image_struct.path, version.filename)
+    end
   end
 
   defp description(%{formatted_short_bio: nil} = user), do: default_description(user)
