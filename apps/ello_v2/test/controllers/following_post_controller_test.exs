1a889712f5f908cf55b5889f12bab21c76fa8192
Colin T.A. Gray <colinta@gmail.com>
2017-03-21 11:56:30 -0600

    added json schema test, moved setup code to setup block

--- apps/ello_v2/test/controllers/following_post_controller_test.exs
+++ apps/ello_v2/test/controllers/following_post_controller_test.exs
@@ -11,10 +11,6 @@ defmodule Ello.V2.FollowingPostControllerTest do
     Stream.Client.Test.reset
 
     user = Factory.insert(:user)
-    {:ok, conn: auth_conn(conn, user), unauth_conn: conn, user: user}
-  end
-
-  test "GET /v2/following/posts/recent", %{conn: conn, user: user} do
     following_user = Factory.insert(:user)
     post = Factory.insert(:post, author: following_user)
     roshi_items = [
@@ -25,6 +21,14 @@ defmodule Ello.V2.FollowingPostControllerTest do
     redis_key = "user:#{user.id}:followed_users_id_cache"
     Redis.command(["SADD", redis_key, following_user.id])
 
+    on_exit fn ->
+      Redis.command(["DEL", redis_key])
+    end
+
+    {:ok, conn: auth_conn(conn, user), unauth_conn: conn, user: user}
+  end
+
+  test "GET /v2/following/posts/recent", %{conn: conn} do
     response = get(conn, following_post_path(conn, :index))
     assert response.status == 200
   end
@@ -32,6 +36,12 @@ defmodule Ello.V2.FollowingPostControllerTest do
   test "GET /v2/following/posts/recent - fails for unauth requests", %{unauth_conn: conn} do
     response = get(conn, following_post_path(conn, :index))
     assert response.status == 401
+  end
+
+  @tag :json_schema
+  test "GET /v2/following/posts/recent - json schema", %{conn: conn} do
+    conn = get(conn, following_post_path(conn, :index))
+    assert :ok = validate_json("post", json_response(conn, 200))
   end
 
 end
