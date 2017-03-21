e6eb2cae722c61b2066be3d5a97254b2e2f87178
Colin T.A. Gray <colinta@gmail.com>
2017-03-21 09:40:35 -0600

    adds tests for FollowingPostController

--- apps/ello_v2/test/controllers/following_post_controller_test.exs
+++ apps/ello_v2/test/controllers/following_post_controller_test.exs
@@ -1,14 +1,37 @@
 defmodule Ello.V2.FollowingPostControllerTest do
   use Ello.V2.ConnCase, async: false
   alias Ello.Core.Repo
+  alias Ello.Stream
+  alias Ello.Stream.Item
+  alias Ello.Core.{Redis}
 
-  setup do
+  setup %{conn: conn} do
     Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+    Stream.Client.Test.start
+    Stream.Client.Test.reset
+
+    user = Factory.insert(:user)
+    {:ok, conn: auth_conn(conn, user), unauth_conn: conn, user: user}
   end
 
-  test "GET /v2/following/posts/recent", %{conn: conn} do
+  test "GET /v2/following/posts/recent", %{conn: conn, user: user} do
+    following_user = Factory.insert(:user)
+    post = Factory.insert(:post, author: following_user)
+    roshi_items = [
+      %Item{id: "#{post.id}", stream_id: "#{following_user.id}", ts: DateTime.utc_now},
+    ]
+    Stream.Client.add_items(roshi_items)
+
+    redis_key = "user:#{user.id}:followed_users_id_cache"
+    Redis.command(["SADD", redis_key, following_user.id])
+
     response = get(conn, following_post_path(conn, :index))
     assert response.status == 200
+  end
+
+  test "GET /v2/following/posts/recent - fails for unauth requests", %{unauth_conn: conn} do
+    response = get(conn, following_post_path(conn, :index))
+    assert response.status == 401
   end
 
 end
