03faa61e49f7c898a0ec46bd6882055508ff4bc0
Alan Peabody <gapeabody@gmail.com>
2017-03-23 09:33:09 -0600

    Explicitly start roshi connection pool.
    Includes some tweakable settings.

--- apps/ello_stream/config/config.exs
+++ apps/ello_stream/config/config.exs
@@ -5,14 +5,15 @@ use Mix.Config
 config :ello_stream, ecto_repos: []
 
 config :ello_stream,
-  prefix: System.get_env("STREAM_SERVICE_PREFIX"),
+  prefix:                System.get_env("STREAM_SERVICE_PREFIX"),
   service_url:           System.get_env("STREAM_SERVICE_URL") || "http://localhost:8080",
   base_slop_factor:      String.to_float(System.get_env("ROSHI_BASE_SLOP_FACTOR") || "1.1"),
   nsfw_slop_factor:      String.to_float(System.get_env("ROSHI_NSFW_SLOP_FACTOR") || "0.15"),
   nudity_slop_factor:    String.to_float(System.get_env("ROSHI_NUDITY_SLOP_FACTOR") || "0.15"),
   block_slop_multiplier: String.to_float(System.get_env("ROSHI_BLOCK_SLOP_MULTIPLIER") || "0.001"),
   max_block_slop_factor: String.to_float(System.get_env("ROSHI_MAX_BLOCK_SLOP_FACTOR") || "1.1"),
-  batches_per_request:   String.to_integer(System.get_env("ROSHI_BATCHES_PER_REQUEST") || "3")
+  batches_per_request:   String.to_integer(System.get_env("ROSHI_BATCHES_PER_REQUEST") || "3"),
+  roshi_pool_size:       String.to_integer(System.get_env("ROSHI_POOL_SIZE") || "100")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- apps/ello_stream/lib/ello_stream/application.ex
+++ apps/ello_stream/lib/ello_stream/application.ex
@@ -1,6 +1,4 @@
 defmodule Ello.Stream.Application do
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,15 +6,15 @@ defmodule Ello.Stream.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: Ello.Stream.Worker.start_link(arg1, arg2, arg3)
-      # worker(Ello.Stream.Worker, [arg1, arg2, arg3]),
+      :hackney_pool.child_spec(:roshi, [timeout: 15_000, max_connections: roshi_pool()])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Ello.Stream.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp roshi_pool do
+    Application.get_env(:ello_stream, :roshi_pool_size)
   end
 end
