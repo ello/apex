e51ea7bf910fa4b70dd01e0c34fbbe8f350c2404
Alan Peabody <gapeabody@gmail.com>
2017-04-25 17:12:15 -0400

    Add recency back to post search.

--- apps/ello_search/lib/post_search.ex
+++ apps/ello_search/lib/post_search.ex
@@ -4,13 +4,15 @@ defmodule Ello.Search.PostSearch do
   alias Ello.Search.{Client, PostIndex, TrendingPost, Page}
   use Timex
 
-  def post_search(%{terms: terms} = opts) do
+  def post_search(opts) do
     opts
     |> build_client_filter_queries
-    |> build_text_content_query(terms)
-    |> build_mention_query(terms)
-    |> build_hashtag_query(terms)
+    |> build_text_content_query(opts[:terms])
+    |> build_mention_query(opts[:terms])
+    |> build_hashtag_query(opts[:terms])
     |> build_pagination_query(opts[:page], opts[:per_page])
+    |> filter_days(opts[:within_days])
+    |> boost_recent(opts[:trending])
     |> search_post_index(opts)
   end
 
@@ -32,7 +34,6 @@ defmodule Ello.Search.PostSearch do
     |> filter_nsfw(opts[:allow_nsfw])
     |> filter_nudity(opts[:allow_nudity])
     |> filter_blocked(opts[:current_user])
-    |> filter_days(opts[:within_days])
   end
 
   defp base_query do
@@ -41,48 +42,58 @@ defmodule Ello.Search.PostSearch do
       size: 10,
       stored_fields: [],
       query: %{
-        bool: %{
-          must:     [],
-          should:   [],
-          filter:   [],
-          must_not: [
-            %{term: %{is_comment: true}},
-            %{term: %{is_hidden:  true}},
-            %{term: %{is_repost:  true}},
-          ]
+        function_score: %{
+          query: %{
+            bool: %{
+              must:     [],
+              should:   [],
+              filter:   [],
+              must_not: [
+                %{term: %{is_comment: true}},
+                %{term: %{is_hidden:  true}},
+                %{term: %{is_repost:  true}},
+              ]
+            }
+          },
+          functions: [],
+          score_mode: "sum",
+          boost_mode: "sum"
         }
       }
     }
   end
 
+  defp build_text_content_query(query, nil), do: query
   defp build_text_content_query(query, terms) do
     text_boost = Application.get_env(:ello_search, :text_content_boost_factor)
-    update_in(query[:query][:bool][:must], &([%{query_string: %{query: terms, fields: ["text_content"], boost: text_boost}} | &1]))
+    update_bool(query, :must, &([%{query_string: %{query: terms, fields: ["text_content"], boost: text_boost}} | &1]))
   end
 
+  defp build_mention_query(query, nil), do: query
   defp build_mention_query(query, terms) do
     mention_boost = Application.get_env(:ello_search, :mention_boost_factor)
-    update_in(query[:query][:bool][:should], &([%{match: %{mentions: %{query: terms, boost: mention_boost}}} | &1]))
+    update_bool(query, :should, &([%{match: %{mentions: %{query: terms, boost: mention_boost}}} | &1]))
   end
 
+  defp build_hashtag_query(query, nil), do: query
   defp build_hashtag_query(query, terms) do
     hashtag_boost = Application.get_env(:ello_search, :hashtag_boost_factor)
-    update_in(query[:query][:bool][:should], &([%{match: %{hashtags: %{query: terms, boost: hashtag_boost}}} | &1]))
+    update_bool(query, :should, &([%{match: %{hashtags: %{query: terms, boost: hashtag_boost}}} | &1]))
   end
 
   defp filter_nsfw(query, true), do: query
   defp filter_nsfw(query, false) do
-    update_in(query[:query][:bool][:must_not], &([%{term: %{is_adult_content: true}} | &1]))
+    update_bool(query, :must_not, &([%{term: %{is_adult_content: true}} | &1]))
   end
 
   defp filter_nudity(query, true), do: query
   defp filter_nudity(query, false) do
-    update_in(query[:query][:bool][:must_not], &([%{term: %{has_nudity: true}} | &1]))
+    update_bool(query, :must_not, &([%{term: %{has_nudity: true}} | &1]))
   end
 
   defp filter_blocked(query, nil), do: query
   defp filter_blocked(query, user) do
-    update_in(query[:query][:bool][:must_not], &([%{terms: %{author_id: user.all_blocked_ids}} | &1]))
+    update_bool(query, :must_not, &([%{terms: %{author_id: user.all_blocked_ids}} | &1]))
   end
 
   defp author_query do
@@ -103,10 +114,14 @@ defmodule Ello.Search.PostSearch do
 
   defp build_author_query(query, nil) do
     author_query = filter_private_authors(author_query())
-    update_in(query[:query][:bool][:filter], &([author_query | &1]))
+    update_bool(query, :filter, &([author_query | &1]))
   end
-  defp build_author_query(query, current_user) do
-    update_in(query[:query][:bool][:filter], &([author_query() | &1]))
+  defp build_author_query(query, _current_user) do
+    update_bool(query, :filter, &([author_query() | &1]))
+  end
+
+  defp filter_private_authors(author_query) do
+    update_in(author_query[:has_parent][:query][:bool][:filter], &([%{term: %{is_public: true}} | &1]))
   end
 
   defp build_pagination_query(query, nil, nil), do: query
@@ -120,16 +135,11 @@ defmodule Ello.Search.PostSearch do
     |> update_in([:from], &(&1 = page * per_page))
     |> update_in([:size], &(&1 = per_page))
   end
-  defp build_pagination_query(query, _), do: query
-
-  defp filter_private_authors(query) do
-    update_in(query[:has_parent][:query][:bool][:filter], &([%{term: %{is_public: true}} | &1]))
-  end
 
   defp build_language_query(query, nil), do: query
   defp build_language_query(query, language) do
     language_boost = Application.get_env(:ello_search, :language_boost_factor)
-    update_in(query[:query][:bool][:should], &([%{term: %{detected_language: %{value: language, boost: language_boost}}} | &1]))
+    update_bool(query, :should, &([%{term: %{detected_language: %{value: language, boost: language_boost}}} | &1]))
   end
 
   defp filter_days(query, nil), do: query
@@ -137,7 +147,21 @@ defmodule Ello.Search.PostSearch do
     date = Timex.now
            |> Timex.shift(days: -within_days)
            |> Timex.format!("%Y-%m-%d", :strftime)
-    update_in(query[:query][:bool][:filter], &([%{range: %{created_at: %{gte: date}}} | &1]))
+    update_bool(query, :filter, &([%{range: %{created_at: %{gte: date}}} | &1]))
+  end
+
+  defp boost_recent(query, true) do
+    recent_boost = %{gauss: %{created_at: %{scale: "3h", offset: "1d"}}, weight: 1}
+    update_in(query[:query][:function_score][:functions], &([recent_boost | &1]))
+  end
+
+  defp boost_recent(query, _) do
+    recent_boost = %{gauss: %{created_at: %{scale: "3d", offset: "1d"}}, weight: 1000}
+    update_in(query[:query][:function_score][:functions], &([recent_boost | &1]))
+  end
+
+  defp update_bool(query, type, fun) do
+    update_in(query[:query][:function_score][:query][:bool][type], fun)
   end
 
   defp search_post_index(query, opts) do
@@ -145,7 +169,8 @@ defmodule Ello.Search.PostSearch do
       results = Client.search(PostIndex.index_name(), [PostIndex.post_doc_type], query).body
     end
 
-    posts = results["hits"]["hits"]
+    posts = results
+            |> get_in(["hits", "hits"])
             |> Enum.map(&(String.to_integer(&1["_id"])))
             |> Content.posts_by_ids(opts)
 
