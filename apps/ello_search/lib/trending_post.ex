a58c314c750850d858e4c9daedceb0e9e8a060e8
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-04-25 17:00:08 -0600

    Only boost post details if trending is true

--- apps/ello_search/lib/trending_post.ex
+++ apps/ello_search/lib/trending_post.ex
@@ -5,8 +5,8 @@ defmodule Ello.Search.TrendingPost do
     |> boost_recent(trending)
     |> boost_comment_count(trending)
     |> boost_repost_count(trending)
-    |> boost_view_count
-    |> boost_love_count
+    |> boost_view_count(trending)
+    |> boost_love_count(trending)
     |> update_score_mode(trending)
   end
 
@@ -21,19 +21,19 @@ defmodule Ello.Search.TrendingPost do
 
   defp boost_comment_count(query, true), do:
     update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "comment_count", modifier: "log1p", factor: 0.3}} | &1]))
-  defp boost_comment_count(query, _), do:
-    update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "comment_count", modifier: "log1p", factor: 0.01}} | &1]))
+  defp boost_comment_count(query, _), do: query
 
   defp boost_repost_count(query, true), do:
     update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "repost_count", modifier: "log1p", factor: 0.8}} | &1]))
-  defp boost_repost_count(query, _), do:
-    update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "repost_count", modifier: "log1p", factor: 0.1}} | &1]))
+  defp boost_repost_count(query, _), do: query
 
-  defp boost_view_count(query), do:
+  defp boost_view_count(query, true), do:
     update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "view_count", modifier: "log1p", factor: 0.00001}} | &1]))
+  defp boost_view_count(query, _), do: query
 
-  defp boost_love_count(query), do:
+  defp boost_love_count(query, true), do:
     update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "love_count", modifier: "log1p", factor: 0.05}} | &1]))
+  defp boost_love_count(query, _), do: query
 
   defp update_score_mode(query, true), do:
     update_in(query[:query][:function_score][:score_mode], &(&1 = "multiply"))
