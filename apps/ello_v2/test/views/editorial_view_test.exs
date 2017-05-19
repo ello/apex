0505146391e396dc613c3b8a3b5623b4449c1e45
Alan Peabody <gapeabody@gmail.com>
2017-05-19 12:30:10 -0400

    Tweak stream kind urls.
    * Default to 5 per page.
    * Use trending for following.
    * Track post views as editorial.

--- apps/ello_v2/test/views/editorial_view_test.exs
+++ apps/ello_v2/test/views/editorial_view_test.exs
@@ -83,7 +83,7 @@ defmodule Ello.V2.EditorialViewTest do
     refute json[:subtitle]
     refute json[:url]
     assert json[:links][:post_stream][:type] == "posts"
-    assert json[:links][:post_stream][:href] == "/api/v2/following"
+    assert json[:links][:post_stream][:href] == "/api/v2/following/posts/trending?stream_source=editorial&per_page=5"
   end
 
   test "editorial.json - following kind - anonymous", context do
@@ -97,7 +97,7 @@ defmodule Ello.V2.EditorialViewTest do
     refute json[:subtitle]
     refute json[:url]
     assert json[:links][:post_stream][:type] == "posts"
-    assert json[:links][:post_stream][:href] == "/api/v2/discover/posts/trending"
+    assert json[:links][:post_stream][:href] == "/api/v2/discover/posts/trending?stream_source=editorial&per_page=5"
   end
 
   test "editorial.json - invite_join kind - authenticated", context do
