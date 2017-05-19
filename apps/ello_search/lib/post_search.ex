72c0bf232244f1aaedc4a035c3abe87bd1fba123
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-05-19 12:09:50 -0600

    Fix compile warnings

--- apps/ello_search/lib/post_search.ex
+++ apps/ello_search/lib/post_search.ex
@@ -56,7 +56,7 @@ defmodule Ello.Search.PostSearch do
   end
 
   defp build_text_content_query(query, %{trending: true}), do: query
-  defp build_text_content_query(query, %{terms: terms} = opts), do:
+  defp build_text_content_query(query, opts), do:
     update_bool(query, :must, &([%{query_string: %{query: filter_terms(opts), default_field: "text_content"}} | &1]))
 
   defp build_mention_query(query, %{trending: true}), do: query
