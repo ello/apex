b5fbea57f779bdb3f19f74e33e930617260ddb4c
Alan Peabody <gapeabody@gmail.com>
2017-05-26 12:24:19 -0400

    Add helper around updating trending functions.

--- apps/ello_search/lib/ello_search/post/trending.ex
+++ apps/ello_search/lib/ello_search/post/trending.ex
@@ -10,67 +10,61 @@ defmodule Ello.Search.Post.Trending do
     |> update_score_mode
   end
 
-  defp boost_recent(%{query: query, trending: true, following: nil, category: nil} = search_struct) do
+  defp boost_recent(%{trending: true, following: nil, category: nil} = search_struct) do
     weight = Application.get_env(:ello_search, :post_trending_recency_weight)
     scale = Application.get_env(:ello_search, :post_trending_recency_scale)
     offset = Application.get_env(:ello_search, :post_trending_recency_offset)
     recent_boost = %{gauss: %{created_at: %{scale: scale, offset: offset}}, weight: weight}
-    updated_query = update_in(query[:query][:function_score][:functions], &([recent_boost | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([recent_boost | &1]))
   end
-  defp boost_recent(%{query: query, trending: true, following: true, category: nil} = search_struct) do
+  defp boost_recent(%{trending: true, following: true, category: nil} = search_struct) do
     weight = Application.get_env(:ello_search, :following_trending_recency_weight)
     scale = Application.get_env(:ello_search, :following_trending_recency_scale)
     offset = Application.get_env(:ello_search, :following_trending_recency_offset)
     recent_boost = %{gauss: %{created_at: %{scale: scale, offset: offset}}, weight: weight}
-    updated_query = update_in(query[:query][:function_score][:functions], &([recent_boost | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([recent_boost | &1]))
   end
-  defp boost_recent(%{query: query, trending: true, following: nil} = search_struct) do
+  defp boost_recent(%{trending: true, following: nil} = search_struct) do
     weight = Application.get_env(:ello_search, :category_trending_recency_weight)
     scale = Application.get_env(:ello_search, :category_trending_recency_scale)
     offset = Application.get_env(:ello_search, :category_trending_recency_offset)
     recent_boost = %{gauss: %{created_at: %{scale: scale, offset: offset}}, weight: weight}
-    updated_query = update_in(query[:query][:function_score][:functions], &([recent_boost | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([recent_boost | &1]))
   end
-  defp boost_recent(%{query: query} = search_struct) do
-    updated_query = update_in(query[:sort], &(&1 = %{created_at: %{order: "desc"}}))
-    %{search_struct | query: updated_query}
+  defp boost_recent(search_struct) do
+    put_in(search_struct.query[:sort], %{created_at: %{order: "desc"}})
   end
 
-  defp boost_comment_count(%{query: query, trending: true} = search_struct) do
+  defp boost_comment_count(%{trending: true} = search_struct) do
     factor = Application.get_env(:ello_search, :post_trending_comment_boost)
-    updated_query = update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "comment_count", modifier: "log1p", factor: factor}} | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([%{field_value_factor: %{field: "comment_count", modifier: "log1p", factor: factor}} | &1]))
   end
   defp boost_comment_count(search_struct), do: search_struct
 
-  defp boost_repost_count(%{query: query, trending: true} = search_struct) do
+  defp boost_repost_count(%{trending: true} = search_struct) do
     factor = Application.get_env(:ello_search, :post_trending_repost_boost)
-    updated_query = update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "repost_count", modifier: "log1p", factor: factor}} | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([%{field_value_factor: %{field: "repost_count", modifier: "log1p", factor: factor}} | &1]))
   end
   defp boost_repost_count(search_struct), do: search_struct
 
-  defp boost_view_count(%{query: query, trending: true} = search_struct) do
+  defp boost_view_count(%{trending: true} = search_struct) do
     factor = Application.get_env(:ello_search, :post_trending_view_boost)
-    updated_query = update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "view_count", modifier: "log1p", factor: factor}} | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([%{field_value_factor: %{field: "view_count", modifier: "log1p", factor: factor}} | &1]))
   end
   defp boost_view_count(search_struct), do: search_struct
 
-  defp boost_love_count(%{query: query, trending: true} = search_struct) do
+  defp boost_love_count(%{trending: true} = search_struct) do
     factor = Application.get_env(:ello_search, :post_trending_love_boost)
-    updated_query = update_in(query[:query][:function_score][:functions], &([%{field_value_factor: %{field: "love_count", modifier: "log1p", factor: factor}} | &1]))
-    %{search_struct | query: updated_query}
+    update_function_score(search_struct, :functions, &([%{field_value_factor: %{field: "love_count", modifier: "log1p", factor: factor}} | &1]))
   end
   defp boost_love_count(search_struct), do: search_struct
 
-  defp update_score_mode(%{query: query, trending: true} = search_struct) do
-    updated_query = update_in(query[:query][:function_score][:score_mode], &(&1 = "multiply"))
-    %{search_struct | query: updated_query}
+  defp update_score_mode(%{trending: true} = search_struct) do
+    put_in(search_struct.query[:query][:function_score][:score_mode], "multiply")
   end
   defp update_score_mode(search_struct), do: search_struct
 
+  defp update_function_score(struct, key, fun) do
+    update_in(struct.query[:query][:function_score][key], fun)
+  end
 end
