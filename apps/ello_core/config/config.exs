4dec1c1c77aaf86fb89c6e8e6fad735ba06e89a7
Jay Zeschin <jay.zeschin@modeset.com>
2017-06-06 21:34:44 -0600

    Set a session-level statement timeout per connection
    Tunable via POSTGRES_STATEMENT_TIMEOUT and defaulted to 1 minute to match prod.
    
    Counterpart to https://github.com/ello/ello/pull/2109.
    
    [#146710477]

--- apps/ello_core/config/config.exs
+++ apps/ello_core/config/config.exs
@@ -5,12 +5,14 @@ use Mix.Config
 # incompatable table of same name and blow up if we don't set this.
 config :ello_core, Ello.Core.Repo,
   migration_source: "ecto_migrations",
-  loggers: [{Ecto.LogEntry, :log, []}, NewRelicPhoenix.Ecto]
+  loggers: [{Ecto.LogEntry, :log, []}, NewRelicPhoenix.Ecto],
+  after_connect: {Ello.Core.Repo, :after_connect, []}
 
 config :ello_core,
   ecto_repos: [Ello.Core.Repo],
   redis_pool_size: String.to_integer(System.get_env("REDIS_POOL_SIZE") || "5"),
   redis_timeout: String.to_integer(System.get_env("REDIS_TIMEOUT") || "10000"),
-  user_post_query_timeout: String.to_integer(System.get_env("USER_POST_QUERY_TIMEOUT") || "10000")
+  user_post_query_timeout: String.to_integer(System.get_env("USER_POST_QUERY_TIMEOUT") || "10000"),
+  pg_statement_timeout: (System.get_env("POSTGRES_STATEMENT_TIMEOUT") || "1min")
 
 import_config "#{Mix.env}.exs"
--- apps/ello_core/lib/ello_core/repo.ex
+++ apps/ello_core/lib/ello_core/repo.ex
@@ -1,3 +1,9 @@
 defmodule Ello.Core.Repo do
   use Ecto.Repo, otp_app: :ello_core
+
+  def after_connect(conn) do
+    timeout = Application.get_env(:ello_core, :pg_statement_timeout)
+    query = "SET SESSION statement_timeout = '#{timeout}'"
+    {:ok, _} = Postgrex.query(conn, query, [])
+  end
 end
