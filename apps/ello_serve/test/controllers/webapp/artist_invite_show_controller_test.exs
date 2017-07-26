cf88290e85939c3a2377bc639728a6653eec71fa
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-07-26 15:01:20 -0600

    Always append a ~ before artist invite lookup
    - We will find by slug 100% of the time

--- apps/ello_serve/test/controllers/webapp/artist_invite_show_controller_test.exs
+++ apps/ello_serve/test/controllers/webapp/artist_invite_show_controller_test.exs
@@ -21,19 +21,8 @@ defmodule Ello.Serve.Webapp.ArtistInviteControllerTest do
   end
 
   @tag :meta
-  test "artist-invites/:id - meta", %{conn: conn, a_inv1: a_inv1} do
-    resp = get(conn, artist_invite_show_path(conn, :show, a_inv1))
-    html = html_response(resp, 200)
-
-    assert html =~ "<title>Foo Brand | Ello</title>"
-    assert has_meta(html, property: "og:title", content: "Foo Brand | Ello")
-    assert has_meta(html, property: "og:description", content: "Bar")
-    assert has_meta(html, name: "robots", content: "index, follow")
-  end
-
-  @tag :meta
   test "artist-invites/:slug - meta", %{conn: conn, a_inv1: a_inv1} do
-    resp = get(conn, artist_invite_show_path(conn, :show, "~#{a_inv1.slug}"))
+    resp = get(conn, artist_invite_show_path(conn, :show, a_inv1.slug))
     html = html_response(resp, 200)
 
     assert html =~ "<title>Foo Brand | Ello</title>"
@@ -42,8 +31,8 @@ defmodule Ello.Serve.Webapp.ArtistInviteControllerTest do
     assert has_meta(html, name: "robots", content: "index, follow")
   end
 
-  test "artist-invites/:id - it renders noscript", %{conn: conn, a_inv1: a_inv1, sub1: sub1, sub2: sub2, sub3: sub3} do
-    resp = get(conn, artist_invite_show_path(conn, :show, a_inv1), %{"per_page" => "2"})
+  test "artist-invites/:slug - it renders noscript", %{conn: conn, a_inv1: a_inv1, sub1: sub1, sub2: sub2, sub3: sub3} do
+    resp = get(conn, artist_invite_show_path(conn, :show, a_inv1.slug), %{"per_page" => "2"})
     html = html_response(resp, 200)
 
     assert html =~ "<title>Foo Brand | Ello</title>"
@@ -60,15 +49,15 @@ defmodule Ello.Serve.Webapp.ArtistInviteControllerTest do
     refute html =~ sub3.post.token
     assert [_, before] = Regex.run(~r'before=(.*)">Next Page</a>', html)
 
-    resp2 = get(conn, artist_invite_show_path(conn, :show, a_inv1), %{"per_page" => "2", "before" => before})
+    resp2 = get(conn, artist_invite_show_path(conn, :show, a_inv1.slug), %{"per_page" => "2", "before" => before})
     html2 = html_response(resp2, 200)
 
     refute html2 == html
     assert html2 =~ sub3.post.token
   end
 
-  test "artist-invites/:id - it renders noscript with selections", %{conn: conn, a_inv2: a_inv2} do
-    resp = get(conn, artist_invite_show_path(conn, :show, a_inv2, %{"per_page" => "2"}))
+  test "artist-invites/:slug - it renders noscript with selections", %{conn: conn, a_inv2: a_inv2} do
+    resp = get(conn, artist_invite_show_path(conn, :show, a_inv2.slug, %{"per_page" => "2"}))
     html = html_response(resp, 200)
     assert html =~ "<h2>Selections</h2>"
   end
--- apps/ello_serve/web/controllers/webapp/artist_invite_show_controller.ex
+++ apps/ello_serve/web/controllers/webapp/artist_invite_show_controller.ex
@@ -2,22 +2,22 @@ defmodule Ello.Serve.Webapp.ArtistInviteShowController do
   use Ello.Serve.Web, :controller
   alias Ello.Core.Contest
 
-  def show(conn, %{"id" => id_or_slug}) do
-    case artist_invite(conn, id_or_slug) do
+  def show(conn, %{"id" => slug}) do
+    case artist_invite(conn, slug) do
       nil    -> send_resp(conn, 404, "")
       invite -> render_html(conn, %{
         artist_invite: invite,
-        submissions: fn -> submissions(conn, id_or_slug) end,
+        submissions: fn -> submissions(conn, slug) end,
       })
     end
   end
 
-  defp artist_invite(conn, id_or_slug),
-    do: Contest.artist_invite(%{id_or_slug: id_or_slug})
+  defp artist_invite(conn, slug),
+    do: Contest.artist_invite(%{id_or_slug: "~#{slug}"})
 
-  defp submissions(conn, id_or_slug) do
+  defp submissions(conn, slug) do
     Contest.artist_invite_submissions(standard_params(conn, %{
-      invite: Contest.artist_invite(%{id_or_slug: id_or_slug}),
+      invite: Contest.artist_invite(%{id_or_slug: "~#{slug}"}),
       status: "approved",
     }))
   end
