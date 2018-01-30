40533d676eaac99a06e014837b3b546bee1d77fc
Alan Peabody <gapeabody@gmail.com>
2018-01-30 08:45:14 -0500

    Add testing helper, use on query introspection.

--- apps/ello_v3/test/ello_v3/schema_test.exs
+++ apps/ello_v3/test/ello_v3/schema_test.exs
@@ -1,3 +1,34 @@
 defmodule Ello.V3.SchemaTest do
   use Ello.V3.Case
+
+  @query_list_query """
+    {
+      __schema {
+        queryType {
+          name
+          fields {
+            name
+          }
+        }
+      }
+    }
+  """
+
+  test "has a post query" do
+    resp = post_graphql(%{query: @query_list_query})
+    json = json_response(resp)["data"]["__schema"]["queryType"]["fields"]
+    assert "post" in Enum.map(json, &(&1["name"]))
+  end
+
+  test "has a userPostStream query" do
+    resp = post_graphql(%{query: @query_list_query})
+    json = json_response(resp)["data"]["__schema"]["queryType"]["fields"]
+    assert "userPostStream" in Enum.map(json, &(&1["name"]))
+  end
+
+  test "has a categoriesPostStream query" do
+    resp = post_graphql(%{query: @query_list_query})
+    json = json_response(resp)["data"]["__schema"]["queryType"]["fields"]
+    assert "categoriesPostStream" in Enum.map(json, &(&1["name"]))
+  end
 end
--- apps/ello_v3/test/support/case.ex
+++ apps/ello_v3/test/support/case.ex
@@ -1,5 +1,7 @@
 defmodule Ello.V3.Case do
   use ExUnit.CaseTemplate
+  use Plug.Test
+  alias Ello.Auth.JWT
 
   using do
     quote do
@@ -14,5 +16,19 @@ defmodule Ello.V3.Case do
 
   setup _tags do
     Ecto.Adapters.SQL.Sandbox.checkout(Ello.Core.Repo)
+  end
+
+  def post_graphql(body, user \\ nil) do
+    jwt = if user, do: JWT.generate(user), else: JWT.generate()
+    :post
+    |> conn("/api/v3/graphql", body)
+    |> put_req_header("content-type", "application/json")
+    |> put_req_header("accepts", "application/json")
+    |> put_req_header("authorization", "Bearer #{jwt}")
+    |> Ello.V3.call([])
+  end
+
+  def json_response(conn) do
+    Poison.decode!(conn.resp_body)
   end
 end
