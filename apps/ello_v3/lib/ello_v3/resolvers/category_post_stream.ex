cb634e43f883efc898a63a00cf71902bd804e307
Justin Holmes <justin.ryan.holmes@icloud.com>
2018-02-09 13:18:40 -0700

    Add category post stream endpoint

--- apps/ello_v3/lib/ello_v3/resolvers/category_post_stream.ex
+++ apps/ello_v3/lib/ello_v3/resolvers/category_post_stream.ex
@@ -1,18 +1,23 @@
 defmodule Ello.V3.Resolvers.CategoryPostStream do
+  alias Ello.Stream
   alias Ello.Core.Discovery
   import Ello.V3.Resolvers.PaginationHelpers
 
-  def call(_, %{id_or_slug: id_or_slug} = args, _resolution) do
-    #Preload here
-    case Ello.Core.Discovery.category(%{id_or_slug: "#{id_or_slug}"}) do
+  def call(_parent, %{id: id} = args, _resolution), do: resolve_category(id, args)
+  def call(_parent, %{slug: slug} = args, _resolution), do: resolve_category(slug, args)
+
+  defp resolve_category(id_or_slug, args) do
+    case Ello.Core.Discovery.category(%{id_or_slug: id_or_slug}) do
       nil -> {:error, "Category not found"}
       category ->
         stream = Stream.fetch(Map.merge(args, %{
-          keys:       stream_key(category),
+          keys:       [stream_key(category)],
           allow_nsfw: true,
         }))
 
         {:ok, %{
+          id: category.id,
+          slug: category.slug,
           posts: stream.posts,
           next: stream.before,
           is_last_page: is_last_page(args, stream.posts)
