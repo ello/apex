8e98a9efa4d569169ba733648fc9acaad3c773e7
Justin Holmes <justin.ryan.holmes@icloud.com>
2018-02-12 16:55:14 -0700

    Add featured kind to subscribed	 post stream

--- apps/ello_v3/lib/ello_v3/resolvers/subscribed_post_stream.ex
+++ apps/ello_v3/lib/ello_v3/resolvers/subscribed_post_stream.ex
@@ -1,8 +1,11 @@
 defmodule Ello.V3.Resolvers.SubscribedPostStream do
   alias Ello.Search.Post.Search
+  alias Ello.Core.Discovery
+  alias Ello.Stream
   import Ello.V3.Resolvers.PaginationHelpers
 
   def call(_parent, %{current_user: nil} = args, _resolver), do: {:error, "Must be logged in"}
+  def call(_, %{kind: :recent}, _), do: {:error, "Recent has not been implemented"}
   def call(_, %{kind: :trending, current_user: current_user} = args, _) do
     search = Search.post_search(Map.merge(args, %{
       page:         trending_page_from_before(args),
@@ -19,4 +22,20 @@ defmodule Ello.V3.Resolvers.SubscribedPostStream do
       is_last_page: search.total_pages == search.page,
     }}
   end
+  def call(_, %{kind: :featured, current_user: current_user} = args, _) do
+    categories = Discovery.categories(%{ids: current_user.followed_category_ids})
+    stream = Stream.fetch(Map.merge(args, %{
+      keys:       Enum.map(categories, &stream_key/1),
+      allow_nsfw: true,
+    }))
+
+    {:ok, %{
+      posts: stream.posts,
+      next:  stream.before,
+      is_last_page: is_last_page(args, stream.posts)
+    }}
+  end
+
+  defp stream_key(%Discovery.Category{roshi_slug: slug}), do: "categories:v1:#{slug}"
+  defp stream_key(_), do: nil
 end
--- apps/ello_v3/test/ello_v3/resolvers/subscribed_post_stream_test.exs
+++ apps/ello_v3/test/ello_v3/resolvers/subscribed_post_stream_test.exs
@@ -1,6 +1,7 @@
 defmodule Ello.V3.Resolvers.SubscribedPostStreamTest do
   use Ello.V3.Case
   alias Ello.Stream
+  alias Ello.Stream.Item
   alias Ello.Search.Post.Index
 
   @query """
@@ -41,8 +42,45 @@ defmodule Ello.V3.Resolvers.SubscribedPostStreamTest do
       "kind" => "TRENDING",
       "perPage" => 3,
     }}, context.user)
+
     assert %{"data" => %{"subscribedPostStream" => json2}} = json_response(resp2)
     assert %{"isLastPage" => true, "next" => nil, "posts" => posts_resp2} = json2
     assert List.duplicate(to_string(context.cat1.id), 3) == Enum.map(posts_resp2, &(hd(&1["categories"])["id"]))
+  end
+
+  test "Featured stream", context do
+    Stream.Client.Test.start
+    Stream.Client.Test.reset
+
+    post1 = Factory.insert(:post, %{category_ids: [context.cat1.id]})
+    post2 = Factory.insert(:post, %{category_ids: [context.cat1.id]})
+    post3 = Factory.insert(:post, %{category_ids: [context.cat1.id]})
+    post4 = Factory.insert(:post, %{category_ids: [context.cat1.id]})
+    post5 = Factory.insert(:post, %{category_ids: [context.cat2.id]})
+    post6 = Factory.insert(:post, %{category_ids: [context.cat2.id]})
+    roshi_items = [
+      %Item{id: "#{post1.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
+      %Item{id: "#{post2.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
+      %Item{id: "#{post3.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
+      %Item{id: "#{post4.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
+      %Item{id: "#{post5.id}", stream_id: "categories:v1:cat2", ts: DateTime.utc_now},
+      %Item{id: "#{post6.id}", stream_id: "categories:v1:cat2", ts: DateTime.utc_now},
+    ]
+    Stream.Client.add_items(roshi_items)
+
+    resp = post_graphql(%{query: @query, variables: %{"kind" => "FEATURED", "perPage" => 3}}, context.user)
+    assert %{"data" => %{"subscribedPostStream" => json}} = json_response(resp)
+    assert %{"isLastPage" => false, "next" => next, "posts" => posts_resp1} = json
+    assert List.duplicate(to_string(context.cat1.id), 3) == Enum.map(posts_resp1, &(hd(&1["categories"])["id"]))
+
+    resp2 = post_graphql(%{query: @query, variables: %{
+      "before" => next,
+      "kind" => "FEATURED",
+      "perPage" => 3,
+    }}, context.user)
+
+    assert %{"data" => %{"subscribedPostStream" => json2}} = json_response(resp2)
+    assert %{"isLastPage" => true, "next" => _, "posts" => posts_resp2} = json2
+    assert [to_string(context.cat1.id)] == Enum.map(posts_resp2, &(hd(&1["categories"])["id"]))
   end
 end
