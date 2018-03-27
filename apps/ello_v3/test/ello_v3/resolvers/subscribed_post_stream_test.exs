564736c3b3d977d65f769059f577ba4c4f7e493d
Alan Peabody <gapeabody@gmail.com>
2018-03-27 17:14:03 -0400

    Add recent and shop to subscribed stream.

--- apps/ello_v3/test/ello_v3/resolvers/subscribed_post_stream_test.exs
+++ apps/ello_v3/test/ello_v3/resolvers/subscribed_post_stream_test.exs
@@ -74,29 +74,33 @@ defmodule Ello.V3.Resolvers.SubscribedPostStreamTest do
   test "Featured stream", context do
     Stream.Client.Test.start
     Stream.Client.Test.reset
+    %{cat1: cat1, cat2: cat2} = context
 
-    post1 = Factory.insert(:featured_category_post, category: context.cat1).post
-    post2 = Factory.insert(:featured_category_post, category: context.cat1).post
-    post3 = Factory.insert(:featured_category_post, category: context.cat1).post
-    post4 = Factory.insert(:featured_category_post, category: context.cat1).post
-    post5 = Factory.insert(:featured_category_post, category: context.cat2).post
-    post6 = Factory.insert(:featured_category_post, category: context.cat2).post
+    post1 = Factory.insert(:featured_category_post, category: cat1).post
+    post2 = Factory.insert(:featured_category_post, category: cat1).post
+    post3 = Factory.insert(:featured_category_post, category: cat1).post
+    post4 = Factory.insert(:featured_category_post, category: cat1).post
+    post5 = Factory.insert(:featured_category_post, category: cat2).post
+    post6 = Factory.insert(:featured_category_post, category: cat2).post
     roshi_items = [
-      %Item{id: "#{post1.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
-      %Item{id: "#{post2.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
-      %Item{id: "#{post3.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
-      %Item{id: "#{post4.id}", stream_id: "categories:v1:cat1", ts: DateTime.utc_now},
-      %Item{id: "#{post5.id}", stream_id: "categories:v1:cat2", ts: DateTime.utc_now},
-      %Item{id: "#{post6.id}", stream_id: "categories:v1:cat2", ts: DateTime.utc_now},
+      %Item{id: "#{post1.id}", stream_id: Stream.key(cat1, :featured), ts: DateTime.utc_now},
+      %Item{id: "#{post2.id}", stream_id: Stream.key(cat1, :featured), ts: DateTime.utc_now},
+      %Item{id: "#{post3.id}", stream_id: Stream.key(cat1, :featured), ts: DateTime.utc_now},
+      %Item{id: "#{post4.id}", stream_id: Stream.key(cat1, :featured), ts: DateTime.utc_now},
+      %Item{id: "#{post5.id}", stream_id: Stream.key(cat2, :featured), ts: DateTime.utc_now},
+      %Item{id: "#{post6.id}", stream_id: Stream.key(cat2, :featured), ts: DateTime.utc_now},
     ]
     Stream.Client.add_items(roshi_items)
 
-    resp = post_graphql(%{query: @query, variables: %{"kind" => "FEATURED", "perPage" => 3}}, context.user)
+    resp = post_graphql(%{query: @query, variables: %{
+      "kind" => "FEATURED",
+      "perPage" => 3
+    }}, context.user)
     assert %{"data" => %{"subscribedPostStream" => json}} = json_response(resp)
     assert %{"isLastPage" => false, "next" => next, "posts" => posts_resp1} = json
     assert length(posts_resp1) === 3
     Enum.each posts_resp1, fn(post_json) ->
-      assert hd(post_json["categories"])["id"] === "#{context.cat1.id}"
+      assert hd(post_json["categories"])["id"] === "#{cat1.id}"
     end
 
     resp2 = post_graphql(%{query: @query, variables: %{
@@ -109,7 +113,99 @@ defmodule Ello.V3.Resolvers.SubscribedPostStreamTest do
     assert %{"isLastPage" => true, "next" => _, "posts" => posts_resp2} = json2
     assert length(posts_resp2) === 1
     Enum.each posts_resp2, fn(post_json) ->
-      assert hd(post_json["categories"])["id"] === "#{context.cat1.id}"
+      assert hd(post_json["categories"])["id"] === "#{cat1.id}"
+    end
+  end
+
+  test "Recent stream", context do
+    Stream.Client.Test.start
+    Stream.Client.Test.reset
+    %{cat1: cat1, cat2: cat2} = context
+
+    post1 = Factory.insert(:category_post, category: cat1).post
+    post2 = Factory.insert(:category_post, category: cat1).post
+    post3 = Factory.insert(:category_post, category: cat1).post
+    post4 = Factory.insert(:category_post, category: cat1).post
+    post5 = Factory.insert(:category_post, category: cat2).post
+    post6 = Factory.insert(:category_post, category: cat2).post
+    roshi_items = [
+      %Item{id: "#{post1.id}", stream_id: Stream.key(cat1, :recent), ts: DateTime.utc_now},
+      %Item{id: "#{post2.id}", stream_id: Stream.key(cat1, :recent), ts: DateTime.utc_now},
+      %Item{id: "#{post3.id}", stream_id: Stream.key(cat1, :recent), ts: DateTime.utc_now},
+      %Item{id: "#{post4.id}", stream_id: Stream.key(cat1, :recent), ts: DateTime.utc_now},
+      %Item{id: "#{post5.id}", stream_id: Stream.key(cat2, :recent), ts: DateTime.utc_now},
+      %Item{id: "#{post6.id}", stream_id: Stream.key(cat2, :recent), ts: DateTime.utc_now},
+    ]
+    Stream.Client.add_items(roshi_items)
+
+    resp = post_graphql(%{query: @query, variables: %{
+      "kind" => "RECENT",
+      "perPage" => 3
+    }}, context.user)
+    assert %{"data" => %{"subscribedPostStream" => json}} = json_response(resp)
+    assert %{"isLastPage" => false, "next" => next, "posts" => posts_resp1} = json
+    assert length(posts_resp1) === 3
+    Enum.each posts_resp1, fn(post_json) ->
+      assert hd(post_json["categories"])["id"] === "#{cat1.id}"
+    end
+
+    resp2 = post_graphql(%{query: @query, variables: %{
+      "before" => next,
+      "kind" => "RECENT",
+      "perPage" => 3,
+    }}, context.user)
+
+    assert %{"data" => %{"subscribedPostStream" => json2}} = json_response(resp2)
+    assert %{"isLastPage" => true, "next" => _, "posts" => posts_resp2} = json2
+    assert length(posts_resp2) === 1
+    Enum.each posts_resp2, fn(post_json) ->
+      assert hd(post_json["categories"])["id"] === "#{cat1.id}"
+    end
+  end
+
+  test "Shop stream", context do
+    Stream.Client.Test.start
+    Stream.Client.Test.reset
+    %{cat1: cat1, cat2: cat2} = context
+
+    post1 = Factory.insert(:category_post, category: cat1).post
+    post2 = Factory.insert(:category_post, category: cat1).post
+    post3 = Factory.insert(:category_post, category: cat1).post
+    post4 = Factory.insert(:category_post, category: cat1).post
+    post5 = Factory.insert(:category_post, category: cat2).post
+    post6 = Factory.insert(:category_post, category: cat2).post
+    roshi_items = [
+      %Item{id: "#{post1.id}", stream_id: Stream.key(cat1, :shop), ts: DateTime.utc_now},
+      %Item{id: "#{post2.id}", stream_id: Stream.key(cat1, :shop), ts: DateTime.utc_now},
+      %Item{id: "#{post3.id}", stream_id: Stream.key(cat1, :shop), ts: DateTime.utc_now},
+      %Item{id: "#{post4.id}", stream_id: Stream.key(cat1, :shop), ts: DateTime.utc_now},
+      %Item{id: "#{post5.id}", stream_id: Stream.key(cat2, :shop), ts: DateTime.utc_now},
+      %Item{id: "#{post6.id}", stream_id: Stream.key(cat2, :shop), ts: DateTime.utc_now},
+    ]
+    Stream.Client.add_items(roshi_items)
+
+    resp = post_graphql(%{query: @query, variables: %{
+      "kind" => "SHOP",
+      "perPage" => 3
+    }}, context.user)
+    assert %{"data" => %{"subscribedPostStream" => json}} = json_response(resp)
+    assert %{"isLastPage" => false, "next" => next, "posts" => posts_resp1} = json
+    assert length(posts_resp1) === 3
+    Enum.each posts_resp1, fn(post_json) ->
+      assert hd(post_json["categories"])["id"] === "#{cat1.id}"
+    end
+
+    resp2 = post_graphql(%{query: @query, variables: %{
+      "before" => next,
+      "kind" => "SHOP",
+      "perPage" => 3,
+    }}, context.user)
+
+    assert %{"data" => %{"subscribedPostStream" => json2}} = json_response(resp2)
+    assert %{"isLastPage" => true, "next" => _, "posts" => posts_resp2} = json2
+    assert length(posts_resp2) === 1
+    Enum.each posts_resp2, fn(post_json) ->
+      assert hd(post_json["categories"])["id"] === "#{cat1.id}"
     end
   end
 end
