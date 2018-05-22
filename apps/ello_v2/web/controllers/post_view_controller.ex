4731b8cdcc7116d0b2b374c9b8ee84b67afcfa57
Alan Peabody <gapeabody@gmail.com>
2018-05-22 15:26:22 -0400

    Handle post view API list handling.
    Tokens and IDs do not come in as an array as recognized by Plug, so
    parse and format as a list as appropriate.

--- apps/ello_v2/web/controllers/post_view_controller.ex
+++ apps/ello_v2/web/controllers/post_view_controller.ex
@@ -35,13 +35,25 @@ defmodule Ello.V2.PostViewController do
     reposted_source: %{author: %{}},
   }
 
+  defp find_posts(%{"posts" => tokens}),
+    do: Content.posts(%{tokens: parse_list(tokens), preloads: @preloads, current_user: nil})
   defp find_posts(%{"post_tokens" => tokens}),
-    do: Content.posts(%{tokens: List.wrap(tokens), preloads: @preloads, current_user: nil})
+    do: Content.posts(%{tokens: parse_list(tokens), preloads: @preloads, current_user: nil})
   defp find_posts(%{"post_ids" => ids}),
-    do: Content.posts(%{ids: List.wrap(ids), preloads: @preloads, current_user: nil})
+  do: Content.posts(%{ids: to_ids(parse_list(ids)), preloads: @preloads, current_user: nil})
   defp find_posts(_), do: nil
 
-  defp safe_string(str) when is_binary(str), 
+  defp parse_list(list) when is_list(list), do: list
+  defp parse_list(string) do
+    string
+    |> String.replace(~r{[\[\(\)\]]}, "")
+    |> String.split(",")
+  end
+
+  defp to_ids([s | _] = ids) when is_binary(s), do: Enum.map(ids, &String.to_integer/1)
+  defp to_ids(ids), do: ids
+
+  defp safe_string(str) when is_binary(str),
     do: if String.valid?(str), do: str, else: nil
   defp safe_string(nil), do: nil
 end
