ec6c5b4d2501ea1f2dd2b616b7e378201eb58b04
Alan Peabody <gapeabody@gmail.com>
2018-06-18 14:35:45 -0400

    Tweak post finding logic.

--- apps/ello_v3/lib/ello_v3/resolvers/comment_stream.ex
+++ apps/ello_v3/lib/ello_v3/resolvers/comment_stream.ex
@@ -7,15 +7,24 @@ defmodule Ello.V3.Resolvers.CommentStream do
   def call(_parent, %{token: token} = args, _resolution), do: resolve_comments(token, args)
 
   defp resolve_comments(id_or_token, args) do
-    case Content.post(%{id_or_token: id_or_token, current_user: args.current_user}) do
+    case find_post(id_or_token, args) do
       nil -> {:error, "Post not found"}
       post -> comments = Content.comments(Map.merge(args, %{post: post}))
-
         {:ok, %{
           comments: comments,
           next:  next_page(comments),
           is_last_page: is_last_page(args, comments),
         }}
     end
+  end
+
+  defp find_post(id_or_token, args) do
+    Content.post(%{
+      id_or_token: id_or_token,
+      current_user: args.current_user,
+      allow_nsfw: true,
+      allow_nudity: true,
+      preloads: %{reposted_source: %{}},
+    })
   end
 end
