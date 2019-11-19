a3200577c7309043a40d4f87a237f1fabf9cf308
Colin T.A. Gray <colinta@gmail.com>
2019-11-19 08:29:03 -0500

    add recaptcha

--- apps/ello_serve/web/views/webapp/meta_view.ex
+++ apps/ello_serve/web/views/webapp/meta_view.ex
@@ -2,6 +2,7 @@ defmodule Ello.Serve.Webapp.MetaView do
   use Ello.Serve.Web, :view
 
   def apple_app_id, do: Application.get_env(:ello_serve, :apple_app_id)
+  def recaptcha_key, do: Application.get_env(:ello_serve, :recaptcha_key)
 
   def current_path(%{path: path}), do: path
   def current_path(%{conn: conn}), do: conn.request_path
@@ -13,6 +14,10 @@ defmodule Ello.Serve.Webapp.MetaView do
 
   def title(%{title: title}), do: title
   def title(_), do: "Ello | The Creators Network"
+
+  def recaptcha_action(%{path: "/join"}), do: 'join'
+  def recaptcha_action(%{path: "/enter"}), do: 'login'
+  def recaptcha_action(_), do: "any"
 
   def description(%{description: description}), do: description
   def description(_), do: "Welcome to the Creators Network. Ello is a community to discover, discuss, publish, share and promote the things you are passionate about."
